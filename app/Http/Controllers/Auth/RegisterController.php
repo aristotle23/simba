d80a973cce895dec6f0be889f3477e6cdf9c516c
Hilary Okoro <aristotle@Hilarys-MacBook-Pro.local>
2022-02-28 12:27:32 +0100

    added user  seed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Account;
 use App\Models\Currency;
 use App\Models\Transaction;
 use App\Providers\RouteServiceProvider;
